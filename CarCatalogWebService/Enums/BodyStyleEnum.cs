e5eb3b6b1329ffb7b6ebb72027222694827653a6
Jasurbek <ismoilov.dzhasurbek02@gmail.com>
2023-09-13 14:44:08 +0500

    BodyStyleEnum.cs file was updated

--- CarCatalogWebService/Enums/BodyStyleEnum.cs
+++ CarCatalogWebService/Enums/BodyStyleEnum.cs
@@ -1,14 +1,5 @@
 namespace CarCatalogWebService.Enums;
 
-/*public static class BodyStyleEnum
-{
-    public static Guid Sedan = Guid.Parse("2b03ae77-d8e2-4e5c-9a6b-66f27e076488");
-    
-    public static Guid Hatchback = Guid.Parse("f4571a87-1587-4a22-9d9e-0e44f9e85557");
-    
-    public static Guid Coupe = Guid.Parse("46729b9d-1ea7-42d9-934e-f5619d988467");
-}*/
-
 public class BodyStyleEnum : BaseEnum<BodyStyleEnum, Guid>
 {
     public static BodyStyleEnum Sedan { get; } =
