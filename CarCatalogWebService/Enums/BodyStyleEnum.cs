cc9e433d38ffdb3bb1f3a43b5fc2b8269dcce08b
Jasurbek <ismoilov.dzhasurbek02@gmail.com>
2023-09-13 14:17:05 +0500

    Enum files was updated

--- CarCatalogWebService/Enums/BodyStyleEnum.cs
+++ CarCatalogWebService/Enums/BodyStyleEnum.cs
@@ -1,10 +1,25 @@
 namespace CarCatalogWebService.Enums;
 
-public static class BodyStyleEnum
+/*public static class BodyStyleEnum
 {
     public static Guid Sedan = Guid.Parse("2b03ae77-d8e2-4e5c-9a6b-66f27e076488");
     
     public static Guid Hatchback = Guid.Parse("f4571a87-1587-4a22-9d9e-0e44f9e85557");
     
     public static Guid Coupe = Guid.Parse("46729b9d-1ea7-42d9-934e-f5619d988467");
+}*/
+
+public class BodyStyleEnum : BaseEnum<BodyStyleEnum, Guid>
+{
+    public static BodyStyleEnum Sedan { get; } =
+        new (Guid.Parse("2b03ae77-d8e2-4e5c-9a6b-66f27e076488"), "Седан");
+    public static BodyStyleEnum Hatchback { get; } =
+        new (Guid.Parse("f4571a87-1587-4a22-9d9e-0e44f9e85557"), "Хэтчбек");
+    public static BodyStyleEnum Coupe { get; } =
+        new (Guid.Parse("46729b9d-1ea7-42d9-934e-f5619d988467"), "Купе");
+    
+    
+    protected BodyStyleEnum(Guid val, string name) : base(val, name)
+    {
+    }
 }
--- CarCatalogWebService/Enums/TransmissionEnum.cs
+++ CarCatalogWebService/Enums/TransmissionEnum.cs
@@ -1,8 +1,14 @@
 namespace CarCatalogWebService.Enums;
 
-public static class TransmissionEnum
+public class TransmissionEnum : BaseEnum<TransmissionEnum, Guid>
 {
-    public static Guid Automatic = Guid.Parse("18f2154f-8ffe-4acc-8692-f223a24b377b");
-    
-    public static Guid Manual = Guid.Parse("a6c95533-dbc4-4152-9bc3-869b94be888a");
+    public static TransmissionEnum Automatic { get; } =
+        new(Guid.Parse("18f2154f-8ffe-4acc-8692-f223a24b377b"), "АКП");
+
+    public static TransmissionEnum Manual { get; } =
+        new(Guid.Parse("a6c95533-dbc4-4152-9bc3-869b94be888a"), "МКП");
+
+    protected TransmissionEnum(Guid val, string name) : base(val, name)
+    {
+    }
 }
