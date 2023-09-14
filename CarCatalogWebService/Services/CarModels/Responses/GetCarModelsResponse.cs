f770ccac3dd09a2c552a84e010623311f909c529
Jasurbek <ismoilov.dzhasurbek02@gmail.com>
2023-09-14 14:08:43 +0500

    GetCarModelsResponse.cs file was add

---
+++ CarCatalogWebService/Services/CarModels/Responses/GetCarModelsResponse.cs
@@ -0,0 +1,10 @@
+namespace CarCatalogWebService.Services.CarModels.Responses;
+
+public class GetCarModelsResponse
+{
+    public Guid Id { get; set; }
+    
+    public string BrandName { get; set; }
+    
+    public string Name { get; set; }
+}
