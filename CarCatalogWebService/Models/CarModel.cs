ec962386315b66598c89165049ae4f22ac758449
Jasurbek <ismoilov.dzhasurbek02@gmail.com>
2023-09-14 16:00:28 +0500

    CarModel.cs file was updated

--- CarCatalogWebService/Models/CarModel.cs
+++ CarCatalogWebService/Models/CarModel.cs
@@ -5,6 +5,7 @@ namespace CarCatalogWebService.Models;
 public class CarModel : AuditableBaseEntity
 {
     public Guid BrandId { get; set; }
+    public  Brand Brand { get; set; }
     
     public string Name { get; set; }
     
