fd42c6a21b9bb31280d3d60fddf46bb21a6e8dd3
Jasurbek <ismoilov.dzhasurbek02@gmail.com>
2023-09-14 16:06:15 +0500

    CarModelMappers was add

---
+++ CarCatalogWebService/Mappers/CarModelMappers/UpdateCarModelMapper.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using CarCatalogWebService.Models;
+using CarCatalogWebService.Services.CarModels.Requests;
+
+namespace CarCatalogWebService.Mappers.CarModelMappers;
+
+public class UpdateCarModelMapper : Profile
+{
+    public UpdateCarModelMapper()
+    {
+        CreateMap<CreateCarModelRequest, CarModel>();
+    }
+}
