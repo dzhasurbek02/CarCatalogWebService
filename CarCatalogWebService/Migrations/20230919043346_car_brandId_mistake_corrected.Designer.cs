2bb2ab0a1375c31bdcbf56eba499b52cd75a3491
Jasurbek <ismoilov.dzhasurbek02@gmail.com>
2023-09-19 17:04:40 +0500

    new migration was add

--- CarCatalogWebService/Migrations/20230919043346_car_brandId_mistake_corrected.Designer.cs
+++ CarCatalogWebService/Migrations/20230919043346_car_brandId_mistake_corrected.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CarCatalogWebService.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CarCatalogWebService.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230919043346_car_brandId_mistake_corrected")]
+    partial class car_brandId_mistake_corrected
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CarCatalogWebService/Migrations/20230919043346_car_brandId_mistake_corrected.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarCatalogWebService.Migrations
+{
+    public partial class car_brandId_mistake_corrected : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Brands_BranId",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CarFeatures_CarId_FeatureId",
+                table: "CarFeatures");
+
+            migrationBuilder.RenameColumn(
+                name: "BranId",
+                table: "Cars",
+                newName: "BrandId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Cars_BranId",
+                table: "Cars",
+                newName: "IX_Cars_BrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarFeatures_CarId",
+                table: "CarFeatures",
+                column: "CarId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Brands_BrandId",
+                table: "Cars",
+                column: "BrandId",
+                principalTable: "Brands",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Brands_BrandId",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CarFeatures_CarId",
+                table: "CarFeatures");
+
+            migrationBuilder.RenameColumn(
+                name: "BrandId",
+                table: "Cars",
+                newName: "BranId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Cars_BrandId",
+                table: "Cars",
+                newName: "IX_Cars_BranId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarFeatures_CarId_FeatureId",
+                table: "CarFeatures",
+                columns: new[] { "CarId", "FeatureId" },
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Brands_BranId",
+                table: "Cars",
+                column: "BranId",
+                principalTable: "Brands",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
